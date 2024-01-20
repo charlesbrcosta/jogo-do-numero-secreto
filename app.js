82530878235f4ab83532dca8793d98b53601672f
Charles Bruno <charlesbrcosta@gmail.com>
2024-01-20 08:55:52 -0300

    criado function verificarChute e limparCampo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirNaTela(tag, texto) {
     let campo = document.querySelector( tag );
@@ -8,6 +9,32 @@ function exibirNaTela(tag, texto) {
 exibirNaTela( 'h1', 'Jogo do número secreto' );
 exibirNaTela( 'p', 'Escolha um número entre 1 e 10' );
 
+limparCampo();
+
+function verificarChute() {
+    let chute = document.querySelector( 'input' ).value;
+
+    if ( chute == numeroSecreto ) {
+        exibirNaTela( 'h1', 'Acertou!' );
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `você descobriu o número secreto com ${ tentativas } ${ palavraTentativa }`
+        exibirNaTela( 'p', mensagemTentativas )
+    } else {
+        if ( chute > numeroSecreto ) {
+            exibirNaTela( 'p', 'O número secreto é menor' );
+        } else {
+            exibirNaTela( 'p', 'O número secreto é maior' )
+        }
+        tentativas ++;
+        limparCampo();
+    }
+}
+
 function gerarNumeroAleatorio() {
-    return Math.floor(Math.random() * 10 + 1);
+    return Math.floor(Math.random() * 1 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector( 'input' );
+    chute.value = '';
 }
